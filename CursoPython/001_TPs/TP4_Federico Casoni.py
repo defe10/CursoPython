46de9cc3ec77919de11975579aac18fdad453885
defe10 <191590799+defe10@users.noreply.github.com>
2025-03-07 20:17:18 -0300

    ok

--- CursoPython/001_TPs/TP4_Federico Casoni.py
+++ CursoPython/001_TPs/TP4_Federico Casoni.py
@@ -1,5 +1,5 @@
 """1. Diseña una función que tome como parámetro 2 números, y que devuelva una lista que contenga TODOS los números enteros entre estos 2 incluyendo AMBOS parámetros.
-Ejemplo: los parámetros para mi función son 1 y 9, por lo tanto, mi función retornara: [1,2,3,4,5,6,7,8,9]
+Ejemplo: los parámetros para mi función son 1 y 9, por lo tanto, mi función retornara: [1,2,3,4,5,6,7,8,9]"""
 
 def num_usuario (numero1, numero2):
     lista = []
@@ -21,8 +21,8 @@ print (num_usuario (12, 9))
 print (num_usuario (9, 12))
 
 
-2. Escribir una función que tome como parámetro 2 números, y retorne una lista con todos los números pares entre estos, EXCLUYENDO a los parámetros.
-Ejemplo: los parámetros son 4 y 9, por lo tanto, la función retornara: [6,8]
+"""2. Escribir una función que tome como parámetro 2 números, y retorne una lista con todos los números pares entre estos, EXCLUYENDO a los parámetros.
+Ejemplo: los parámetros son 4 y 9, por lo tanto, la función retornara: [6,8]"""
 
 
 def num_usuario (numero1, numero2):
@@ -55,8 +55,8 @@ print (num_usuario (2, 9))
 print (num_usuario (-9,1))
 
 
-3. Escribir una función que tome 2 parámetros, el primero que reciba una cadena, y el segundo que reciba un carácter. La función tendrá que retornar la cantidad de veces que aparece ese carácter en esa cadena.
-Ejemplo: si los parámetros son “Hola mi nombre es Sebastián” y “s”, la función tendrá que retornar 3 ya que la “s” se encuentra 3 veces repetidas en mi cadena.
+"""3. Escribir una función que tome 2 parámetros, el primero que reciba una cadena, y el segundo que reciba un carácter. La función tendrá que retornar la cantidad de veces que aparece ese carácter en esa cadena.
+Ejemplo: si los parámetros son “Hola mi nombre es Sebastián” y “s”, la función tendrá que retornar 3 ya que la “s” se encuentra 3 veces repetidas en mi cadena."""
 
 def cantidad (cadena, caracter):
     contador = 0
@@ -75,8 +75,8 @@ veces = cantidad (frase_usuario,letra_usuario)
 
 print (f"{frase_usuario} contiene {veces} veces la letra {letra_usuario}")
 
-4. Elaborar una función que tome como parámetro 2 números, y retorne una lista con todos los números primos entre ese rango de números.
-Ejemplo: mis parámetros son 4 y 45, la función tendrá que retornar: [5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43].
+"""4. Elaborar una función que tome como parámetro 2 números, y retorne una lista con todos los números primos entre ese rango de números.
+Ejemplo: mis parámetros son 4 y 45, la función tendrá que retornar: [5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]."""
 
 def verificar_primo(numero):
 
@@ -107,7 +107,7 @@ lista_entrega = primos(num1_usuario, num2_usuario)
 print(lista_entrega)
 
 
-5. Elaborar una función que tome como parámetro una lista, y devuelva un bool que diga si en esa lista TODOS sus números son pares.
+"""5. Elaborar una función que tome como parámetro una lista, y devuelva un bool que diga si en esa lista TODOS sus números son pares."""
 
 def validar_pares (lista_num_usuario):
  
@@ -128,7 +128,7 @@ num_usuario_pares = validar_pares (lista)
 
 print (num_usuario_pares)
 
-6. Elaborar una función que tome como parámetro una lista y devuelva un bool que diga si en esa lista TODOS sus números son primos.
+"""6. Elaborar una función que tome como parámetro una lista y devuelva un bool que diga si en esa lista TODOS sus números son primos."""
 
 def verificar_primo(numero):
 
@@ -159,7 +159,7 @@ num_usuario_primos = validar_primos (lista)
 print (num_usuario_primos)
 
 
-1. Hacer una calculadora, que tenga las siguientes operaciones.
+"""1. Hacer una calculadora, que tenga las siguientes operaciones.
 a. Suma
 b. Resta
 c. Multiplicación
@@ -202,10 +202,12 @@ def div (lista_div):
     
     return resultado
 
-# def pot (lista_pot):
-   
-#    for i in range (0,len(lista_pot)):
-
+def pot(lista_pot):
+    
+    resultado = lista_pot[0]
+    for numero in lista_pot[1:]:
+        resultado **= numero
+    return resultado
 
 
 
@@ -214,21 +216,38 @@ def div (lista_div):
 
 
 while True:
- op_usuario = input("Ingrese una operación (+,-,*,/,**): ")
- num_usuario = float(input("Ingrese un número: "))
- num_usuario = [num_usuario]
+    op_usuario = input("Ingrese una operación (+, -, *, /, **) o 'SALIR' para terminar: ")
+
+
+    if op_usuario == "SALIR":
+        print("Adiós")
+        break
+
+
+    
+    numeros = []
+    while True:
+        num = input("Ingrese un número (o 'fin' para terminar): ")
+        if num == "fin":
+            break
+   
+    num = float(num)  # Convertir a número
+    numeros.append(num)
+       
+
+
  
 
   
- if op_usuario == "+":
+if op_usuario == "+":
     resultado = suma(num_usuario)
- elif op_usuario == "-":
+elif op_usuario == "-":
     resultado = resta(num_usuario)
- elif op_usuario == "*":
+elif op_usuario == "*":
     resultado = mult(num_usuario)
- elif op_usuario == "/":
+elif op_usuario == "/":
     resultado = div(num_usuario)
- elif op_usuario == "**":
+elif op_usuario == "**":
     resultado = pot(num_usuario)
 
 
@@ -240,9 +259,7 @@ while True:
 
 
 
+# No tuve tiempo de terminar, pero creo que estaba cerca.
 
 
-# lista = [15,-4, 22]
-# resultado = div(lista)
-# print(resultado)
 
